6986cf75912dfeb7a023426fddaf9af3e282b0c6
unknown <fe97964@gmail.com>
2020-06-25 10:12:41 -0300

    Fibonacci feito

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursividade rec = new Recursividade();
+            rec.GerarSequenciaFibonacci(0, 1, 10);
+        }
+    }
+}
---
+++ Recursividade.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Aula25
+{
+    public class Recursividade
+    {
+        //Fibonacci
+     public void GerarSequenciaFibonacci(int num1, int num2, int vezes){
+         if(vezes > 0){
+             Console.WriteLine(num1);
+             GerarSequenciaFibonacci(num2, num1 + num2, vezes - 1);
+         }
+     }   
+    }
+}
